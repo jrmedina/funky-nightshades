c2cdb3fed01ac9101f8e36088176d3c5d1332206
jmedina <ndgns2@gmail.com>
2022-08-25 18:13:40 -0600

    Add/ minimal styling to display poster rows

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,20 @@
-import './App.css';
+import "./App.css";
 import React, { Component } from "react";
-import movieData from '../movieData'
-import MovieContainer from '../MovieContainer/MovieContainer';
+import movieData from "../movieData";
+import MovieContainer from "../MovieContainer/MovieContainer";
 
 class App extends Component {
   constructor() {
-    super()
-    this.state = { movies: movieData.movies }
+    super();
+    this.state = { movies: movieData.movies };
   }
   render() {
-    return <div><MovieContainer movieData={this.state.movies} /></div>;
+    return (
+      <div className="App">
+        <h1>Funky Nightshades</h1>
+        <MovieContainer movieData={this.state.movies} />
+      </div>
+    );
   }
 }
 
--- src/MiniMovieCards/MiniMovieCards.js
+++ src/MiniMovieCards/MiniMovieCards.js
@@ -1,8 +1,15 @@
 import React from "react";
 import MovieContainer from "../MovieContainer/MovieContainer";
+import "./MiniMovieCards.css";
 
 const MiniMovieCards = ({ id, title, poster, rating }) => {
-    return <div><h1>{title}</h1><img src={poster} /><p1>{rating}</p1></div>
-}
+  return (
+    <div className="MiniMovieCard">
+      <h2>{title}</h2>
+      <img className="poster"src={poster} />
+      <p1>{rating.toFixed(2)}⭐️</p1>
+    </div>
+  );
+};
 
 export default MiniMovieCards;
