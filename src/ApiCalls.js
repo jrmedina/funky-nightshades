8122fad3f8107c1670515373627ad544101ea4b3
Michael Martinelli <turtelboy123@gmail.com>
2022-08-27 18:23:22 -0600

    finish fetch request for single posterview

--- src/ApiCalls.js
+++ src/ApiCalls.js
@@ -1,14 +1,14 @@
-const apiCall = () => {
-    return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies').then(response => {
+const apiCall = (data) => {
+    return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/${data}`).then(response => {
         if (response.ok) {
             return response.json()
         } throw Error(response.statusText)
     })
 }
-const getData = () => {
-    let promiseVar = Promise.all([apiCall()])
-        .then(data => {
-            return data;
+const getData = (data) => {
+    let promiseVar = Promise.all([apiCall(data)])
+        .then(movieData => {
+            return movieData;
         })
     return promiseVar;
 }
--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import React, { Component } from "react";
-import movieData from "../movieData";
 import MovieContainer from "../MovieContainer/MovieContainer";
 import SpecificMovieCard from "../SpecificMovieCard/SpecificMovieCard";
 import { getData } from "../ApiCalls";
@@ -12,23 +11,29 @@ class App extends Component {
   }
 
   getDetails = (event) => {
-    let clicked = this.state.movies.filter(
-      (movie) => movie.id === parseInt(event.target.id)
-    );
-    this.setState({ clickedImg: clicked });
+    let movieId = `/movies/${parseInt(event.target.id)}`;
+    getData(movieId).then(data => {
+      console.log(data);
+      this.setState({ clickedImg: new Array(data[0].movie) })
+    })
+    // let clicked = this.state.movies.filter(
+    //   (movie) => movie.id === parseInt(event.target.id)
+    // );
+    // this.setState({ clickedImg: clicked });
   };
 
   resetState = () => {
     this.setState({ clickedImg: [] });
   }
   componentDidMount = () => {
-    getData()
+    getData('/movies')
       .then(data => {
         this.setState({ movies: [...data[0].movies] })
         console.log(`this.state.movies`, this.state.movies)
       })
   }
   render() {
+    console.log('this CLICKED IMG', this.state.clickedImg)
     return (
       <div className="App">
         <h1>Funky Nightshades</h1>
